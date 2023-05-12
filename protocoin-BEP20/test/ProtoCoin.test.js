7a528149ac11e902ace05af96b9dac9f70cbb63a
Patrick Longo <ck.longo@bol.com.br>
2023-05-11 23:15:40 -0300

    starting protocoin mint frontend

--- protocoin-BEP20/test/ProtoCoin.test.js
+++ protocoin-BEP20/test/ProtoCoin.test.js
@@ -1,107 +1,206 @@
 const ProtoCoin = artifacts.require("ProtoCoin");
-const BN = require("bn.js");
+const { BN, time } = require('@openzeppelin/test-helpers');
 
-contract("ProtoCoin", function (accounts) {
+contract('ProtoCoin', function (accounts) {
+
   const DECIMALS = new BN(18);
-  const TOTAL_SUPPLY = new BN(1000).mul(new BN(10).pow(DECIMALS));
 
   beforeEach(async () => {
     contract = await ProtoCoin.new();
-  });
+  })
 
-  it("should has correct NAME", async () => {
+  it("Should has correct name", async () => {
     const name = await contract.name();
-    assert(name === "ProtoCoin", "Incorrect coin name");
+    assert(name === 'New ProtoCoin', 'Incorrect name');
   });
 
-  it("should has correct SYMBOL", async () => {
+  it("Should has correct symbol", async () => {
     const symbol = await contract.symbol();
-    assert(symbol === "PRC", "Incorrect coin symbol");
+    assert(symbol === 'NPC', 'Incorrect symbol');
   });
 
-  it("should has correct DECIMALS", async () => {
+  it("Should has correct decimals", async () => {
     const decimals = await contract.decimals();
-    assert(decimals.eq(DECIMALS), "Incorrect coin decimals");
+    assert(decimals.eq(DECIMALS), 'Incorrect decimals');
   });
 
-  it("should has correct TOTALSUPPLY", async () => {
+  it("Should has correct total supply", async () => {
+    const TOTAL_SUPPLY = new BN(10000000).mul(new BN(10).pow(DECIMALS));
     const totalSupply = await contract.totalSupply();
-    assert(totalSupply.eq(TOTAL_SUPPLY), "Incorrect coin totalSupply");
+    assert(totalSupply.eq(TOTAL_SUPPLY), 'Incorrect totalSupply');
   });
 
-  it("owner should has totalSupply", async () => {
-    const ownerSupply = await contract.balanceOf(accounts[0]);
-    assert(ownerSupply.eq(TOTAL_SUPPLY), "Owner don't have total supply");
+  it("Owner should has total supply", async () => {
+    const TOTAL_SUPPLY = new BN(10000000).mul(new BN(10).pow(DECIMALS));
+    const balance = await contract.balanceOf(accounts[0]);
+    assert(balance.eq(TOTAL_SUPPLY), 'Incorrect balance');
   });
 
-  it("should transfer", async () => {
+  it("Should transfer", async () => {
     const qty = new BN(1).mul(new BN(10).pow(DECIMALS));
-    const tx = await contract.transfer(accounts[1], qty);
 
-    const fromBalance = await contract.balanceOf(accounts[0]);
-    const toBalance = await contract.balanceOf(accounts[1]);
+    const balanceAdminBefore = await contract.balanceOf(accounts[0]);
+    const balanceToBefore = await contract.balanceOf(accounts[1]);
 
-    assert(tx.receipt.status === true, "Transfer not completed");
-    assert(fromBalance.eq(TOTAL_SUPPLY.sub(qty)), "Incorrect from balance");
-    assert(toBalance.eq(qty), "Incorrect to balance");
+    await contract.transfer(accounts[1], qty);
+
+    const balanceAdminNow = await contract.balanceOf(accounts[0]);
+    const balanceToNow = await contract.balanceOf(accounts[1]);
+
+    assert(balanceAdminNow.eq(balanceAdminBefore.sub(qty)), 'Incorrect admin balance');
+    assert(balanceToNow.eq(balanceToBefore.add(qty)), 'Incorrect to balance');
   });
 
-  it("should NOT transfer", async () => {
-    const qty = new BN(1001).mul(new BN(10).pow(DECIMALS));
+  it("Should NOT transfer", async () => {
+    const qty = new BN(10000001).mul(new BN(10).pow(DECIMALS));
 
     try {
       await contract.transfer(accounts[1], qty);
-      assert.fail("The transfer should have trhown an error");
-    } catch (error) {
-      assert.include(
-        error.message,
-        "revert",
-        "The transfer should be reverted"
-      );
+      assert.fail("The transfer should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The transfer should be reverted.");
     }
   });
 
-  it("should approve", async () => {
+  it("Should approve", async () => {
     const qty = new BN(1).mul(new BN(10).pow(DECIMALS));
     await contract.approve(accounts[1], qty);
 
     const allowance = await contract.allowance(accounts[0], accounts[1]);
-
-    assert(allowance.eq(qty), "Incorrect allowance balance");
+    assert(allowance.eq(qty), 'Incorrect allowance balance');
   });
 
-  it("should transfer from (allowance)", async () => {
+  it("Should transfer from", async () => {
     const qty = new BN(1).mul(new BN(10).pow(DECIMALS));
 
-    await contract.approve(accounts[1], qty.mul(new BN(2)));
-    const tx = await contract.transferFrom(accounts[0], accounts[2], qty, {
-      from: accounts[1],
-    });
+    const allowanceBefore = await contract.allowance(accounts[0], accounts[1]);
+    const balanceAdminBefore = await contract.balanceOf(accounts[0]);
+    const balanceToBefore = await contract.balanceOf(accounts[1]);
 
-    const ownerBalance = await contract.balanceOf(accounts[0]);
-    const toBalance = await contract.balanceOf(accounts[2]);
-    const allowanceUpdated = await contract.allowance(accounts[0], accounts[1]);
+    await contract.approve(accounts[1], qty);
+    await contract.transferFrom(accounts[0], accounts[1], qty, { from: accounts[1] });
 
-    assert(tx.receipt.status === true, "Transfer not completed");
-    assert(toBalance.eq(qty), "Incorrect to balance");
-    assert(ownerBalance.eq(TOTAL_SUPPLY.sub(qty)), "Incorrect owner balance");
-    assert(allowanceUpdated.eq(qty), "Incorrect allowance remaining balance");
+    const allowanceNow = await contract.allowance(accounts[0], accounts[1]);
+    const balanceAdminNow = await contract.balanceOf(accounts[0]);
+    const balanceToNow = await contract.balanceOf(accounts[1]);
+
+    assert(allowanceNow.eq(allowanceBefore), 'Incorrect allowance');
+    assert(balanceAdminNow.eq(balanceAdminBefore.sub(qty)), 'Incorrect admin balance');
+    assert(balanceToNow.eq(balanceToBefore.add(qty)), 'Incorrect to balance');
   });
 
-  it("should NOT transfer from (allowance)", async () => {
+  it("Should NOT transfer from", async () => {
     const qty = new BN(1).mul(new BN(10).pow(DECIMALS));
 
     try {
-      await contract.transferFrom(accounts[0], accounts[2], qty, {
-        from: accounts[1],
-      });
-      assert.fail("The transfer from (allowance) should have trhown an error");
-    } catch (error) {
-      assert.include(
-        error.message,
-        "revert",
-        "The transfer from (allowance) should be reverted"
-      );
+      await contract.transferFrom(accounts[0], accounts[1], qty, { from: accounts[1] });
+      assert.fail("The transfer should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The transferFrom should be reverted.");
+    }
+  });
+
+  it("Should mint once", async () => {
+    const mintAmount = new BN(1000);
+    await contract.setMintAmount(mintAmount);
+
+    const balanceBefore = await contract.balanceOf(accounts[1]);
+    await contract.mint(accounts[1], { from: accounts[0] });
+    const balanceNow = await contract.balanceOf(accounts[1]);
+
+    assert(balanceNow.eq(balanceBefore.add(mintAmount)), 'Incorrect balance');
+  });
+
+  it("Should mint twice (different accounts)", async () => {
+    const mintAmount = new BN(1000);
+    await contract.setMintAmount(mintAmount);
+
+    const balance1Before = await contract.balanceOf(accounts[1]);
+    const balance2Before = await contract.balanceOf(accounts[2]);
+
+    await contract.mint(accounts[1], { from: accounts[0] });
+    await contract.mint(accounts[2], { from: accounts[0] });
+
+    const balance1Now = await contract.balanceOf(accounts[1]);
+    const balance2Now = await contract.balanceOf(accounts[2]);
+
+    assert(balance1Now.eq(balance1Before.add(mintAmount)), 'Incorrect balance');
+    assert(balance2Now.eq(balance2Before.add(mintAmount)), 'Incorrect balance');
+  });
+
+  it("Should mint twice (different moments)", async () => {
+    const mintAmount = new BN(1000);
+    await contract.setMintAmount(mintAmount);
+
+    const delayInSeconds = 1;
+    await contract.setMintDelay(delayInSeconds);
+
+    const balanceBefore = await contract.balanceOf(accounts[1]);
+    await contract.mint(accounts[1], { from: accounts[0] });
+
+    await time.increase(delayInSeconds * 2);
+
+    await contract.mint(accounts[1], { from: accounts[0] });
+    const balanceNow = await contract.balanceOf(accounts[1]);
+
+    assert(balanceNow.eq(balanceBefore.add(new BN(mintAmount * 2))), 'Incorrect balance');
+  });
+
+  it("Should NOT setMintAmount (permission)", async () => {
+    try {
+      await contract.setMintAmount(1000, { from: accounts[1] });
+      assert.fail("The setMintAmount should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The setMintAmount should be reverted.");
+    }
+  });
+
+  it("Should NOT setMintDelay (permission)", async () => {
+    try {
+      await contract.setMintDelay(1000, { from: accounts[1] });
+      assert.fail("The setMintDelay should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The setMintDelay should be reverted.");
+    }
+  });
+
+  it("Should NOT mint (disabled)", async () => {
+    try {
+      await contract.mint(accounts[1], { from: accounts[0] });
+      assert.fail("The mint should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The mint should be reverted.");
+    }
+  });
+
+  it("Should NOT mint twice", async () => {
+    await contract.setMintAmount(new BN(1000));
+
+    await contract.mint(accounts[1], { from: accounts[0] });
+
+    try {
+      await contract.mint(accounts[1], { from: accounts[0] });
+      assert.fail("The mint should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The mint should be reverted.");
+    }
+  });
+
+  it("Should NOT mint (not owner)", async () => {
+    await contract.setMintAmount(new BN(1000));
+
+    try {
+      await contract.mint(accounts[1], { from: accounts[1] });
+      assert.fail("The mint should have thrown an error.");
+    }
+    catch (err) {
+      assert.include(err.message, "revert", "The mint should be reverted.");
     }
   });
 });
--- protocoin-BEP20/truffle-config.js
+++ protocoin-BEP20/truffle-config.js
@@ -1,27 +1,25 @@
-require("dotenv").config();
-const HDWalletProvider = require("@truffle/hdwallet-provider")
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 
+require('dotenv').config();
 module.exports = {
-  plugins: [
-    "truffle-plugin-verify",
-  ],
+  plugins: ["truffle-plugin-verify"],
   api_keys: {
-    bscscan: process.env.BSCSCAN_API_KEY,
+    bscscan: process.env.BSCSCAN_API_KEY
   },
   networks: {
-    bnbtestnet: {
+    bsctest: {
       provider: new HDWalletProvider({
         mnemonic: {
-          phrase: process.env.SECRET,
+          phrase: process.env.SECRET
         },
         providerOrUrl: process.env.BNB_TESTNET_URL
       }),
-      network_id: "*",
+      network_id: "97"
     },
   },
   compilers: {
     solc: {
-      version: "0.8.19",
+      version: "0.8.17",
       settings: {
         optimizer: {
           enabled: true, // Default: false
