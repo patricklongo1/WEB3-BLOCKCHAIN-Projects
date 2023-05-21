e9782eefb1fecabf0a8b3ea7262bc11019251fda
Patrick Longo <ck.longo@bol.com.br>
2023-05-21 15:22:40 -0300

    front adusts

--- protocoin-faucet-front/src/app/components/LinkCards.tsx
+++ protocoin-faucet-front/src/app/components/LinkCards.tsx
@@ -8,17 +8,17 @@ const LinkCards = ({ href, title, content }: LinkCardsProps) => {
   return (
     <a
       href={href}
-      className="flex flex-col items-center justify-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-700 hover:bg-gray-800 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+      className="flex flex-col items-center max-w-sm justify-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-700 hover:bg-gray-800 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
       target="_blank"
       rel="noopener noreferrer"
     >
-      <h2 className={`mb-3 text-2xl font-semibold text-gray-200`}>
+      <h2 className="mb-3 text-xl sm:text-2xl font-semibold text-gray-200">
         {title}{' '}
         <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
           -&gt;
         </span>
       </h2>
-      <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-gray-200`}>
+      <p className="m-0 max-w-[30ch] text-xs sm:text-sm opacity-50 text-gray-200">
         {content}
       </p>
     </a>
--- protocoin-faucet-front/src/app/page.tsx
+++ protocoin-faucet-front/src/app/page.tsx
@@ -4,7 +4,7 @@ import LinkCards from './components/LinkCards'
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-4 sm:p-24">
-      <div className="w-full relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#00ffff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#00ffff] after:dark:opacity-40 before:lg:h-[360px]">
         <h1 className="text-4xl text-zinc-900 dark:text-zinc-300 font-extrabold text-shadow text-center w-full">
           New ProtoCoin - NPC
         </h1>
@@ -15,13 +15,13 @@ export default async function Home() {
           Get your FAUCETS ProtoCoins
         </h2>
         <p className="text-zinc-900 dark:text-zinc-300 text-sm mb-6 text-center">
-          Once a 24h, earn 0.01 coins for free just connecting your MetaMask
+          Once a 24h, earn 0.1 coins for free just connecting your MetaMask
           below.
         </p>
         <MetamaskConnect />
       </section>
 
-      <div className="w-full mb-4 sm:mb-32 grid text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
+      <div className="w-full flex justify-center space-x-10 mb-4 sm:mb-32 text-center lg:mb-0 lg:grid-cols-2 lg:text-left">
         <LinkCards
           href="https://testnet.bscscan.com/address/0xd47964eb2062b555cb76c41ba8823ebddd3c0d41#code"
           title="Contrat"
