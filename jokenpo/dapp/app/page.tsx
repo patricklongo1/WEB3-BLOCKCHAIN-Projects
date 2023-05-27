133b83ac80e316181f37a67afa6f34919def1e7e
Patrick Longo <ck.longo@bol.com.br>
2023-05-27 00:46:42 -0300

    typography for page titles

--- jokenpo/dapp/app/page.tsx
+++ jokenpo/dapp/app/page.tsx
@@ -1,7 +1,9 @@
 export default function IndexPage() {
   return (
     <section className="container flex w-full flex-col items-center justify-center p-20">
-      <h1>home page</h1>
+      <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        home page
+      </h1>
     </section>
   )
 }
